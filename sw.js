17bc9dce108cf125cbcd82aec515e753951fad6a
hyperhindu123-droid <hyperhindu123@gmail.com>
2026-03-09 16:14:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,35 @@
-const CACHE_NAME = 'mocknova-v1';
+const CACHE_NAME = 'mocknova-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './logo.png'
+  './logo.png.jpeg'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
       })
+    ))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      // अगर इंटरनेट नहीं है, तो ऐप को हैंग होने से बचाए
+      return response || fetch(event.request).catch(() => {
+        return new Response(''); 
+      });
+    })
   );
 });
